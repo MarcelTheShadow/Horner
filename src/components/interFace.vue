b289202aa7d128598fbfd3ab2e8cf19acd26407d
MarcelTheShadow <185245624+MarcelTheShadow@users.noreply.github.com>
2025-04-25 22:46:07 +0200

    working on input-method with regEx-matches to get values easily

--- src/components/interFace.vue
+++ src/components/interFace.vue
@@ -7,13 +7,15 @@ const eingabe = ref('');
 // Durch Button aktualisierter Wert des Inputs
 const polynomOnClick = ref('');
 
-// Einzelner Term aus dem Input
-const term = ref('');
-
 // Ist Input eine gültige Polynomfunktion?
 const polynomGueltig = ref(false);
 
+// Einzelner Term aus dem Input
+const term = ref('');
+const terme = ref([]);
+
 // Funktion, die den Wert des Inputs aktualisiert und überprüft, ob es sich um eine gültige Polynomfunktion handelt
+
 const funktionGueltigOderNicht = (input) => {
     polynomOnClick.value = input;
     if (checkeRegEx(polynomOnClick.value)) {
@@ -24,57 +26,37 @@ const funktionGueltigOderNicht = (input) => {
 };
 
 // Überprüft, ob der Input eine gültige Polynomfunktion ist
+
 const checkeRegEx = (input) => {
     // Regulärer Ausdruck einer Polynomfunktion
     const POLYNOM_REGEX = /^\s*[+-]?\s*\d*\s*(\*?\s*x(\s*\^\s*\d+)?)?(\s*[+-]\s*\d*\s*(\*?\s*x(\s*\^\s*\d+)?)?\s*)*$/;
     return POLYNOM_REGEX.test(input);
 }
 
-// Sortiert einzelnen Term ordentlich und liest Skalar und Exponent aus
-const termAuslesenUndAnordnen = (input) => {
-    let skalar = '';
-    let exponent = '';
-    let positive = true;
+// Liest einzelne Terme und Werte aus RegEx aus
 
-    let skalarInt = 1;
+const polynomfunktionAuslesen = (input) => {
+    terme.value = [];
 
+    // Fügt dem Input ein '+' vor, wenn der erste Buchstabe kein '+' oder '-' ist, damit erster Term auch erkannt wird
     let i = 0;
-    while (input[i] == ' ' && i < input.length) {
+    while (i < input.length) {
         i++;
-    }
-    if (/\d/.test(input[i])) {
-        const anfangIndexSkalar = i;
-        while (/\d/.test(input[i]) && i < input.length) {
-            i++;
+        if(input[i] === ' '){
+            input = input.slice(0, i) + input.slice(i + 1);
         }
-        const endeIndexSkalar = i;
-        skalar = input.slice(anfangIndexSkalar, endeIndexSkalar);
-        skalarInt = parseInt(skalar);
     }
-    while (input[i] == ' ' || input[i] == '*' && i < input.length) {
-        i++;
+    if (input[0] != '+' && input[0] != '-') {
+        input = '+' + input;
     }
-    if (input[i] == 'x') {
-        i++;
+
+    const einzelnerTerm = /[+-]\d*\*x\^\d+/g;
+
+    for(const match of input.matchAll(einzelnerTerm)) {
+       if(match){
+            terme.value += match;
+       }
     }
-    while (input[i] == ' ' && i < input.length) {
-        i++;
-    }
-    if (input[i] == '^') {
-        i++;
-    }
-    while (input[i] == ' ' && i < input.length) {
-        i++;
-    }
-    if (/\d/.test(input[i])) {
-        const anfangIndexExponent = i;
-        while (/\d/.test(input[i]) && i < input.length) {
-            i++;
-        }
-        const endeIndexExponent = i;
-        exponent = input.slice(anfangIndexExponent, endeIndexExponent);
-    }
-    term.value = skalar + "x" + " ^ " + exponent;
 }
 
 </script>
@@ -83,14 +65,14 @@ const termAuslesenUndAnordnen = (input) => {
     <div class="view">
         <input type="text" v-model="eingabe" placeholder="Gib hier deine Zahl ein" />
         <button @click="funktionGueltigOderNicht(eingabe)">Berechne</button>
-        <button @click="termAuslesenUndAnordnen(eingabe)">Term auslesen</button>
+        <button @click="polynomfunktionAuslesen(eingabe)">Term auslesen</button>
     </div>
     <div class="anzeige">
         <p>Die Funktion ist: {{ polynomOnClick }}</p>
         <p v-if="polynomGueltig">Die Funktion ist gültig</p>
         <p v-else>Die Funktion ist ungültig</p>
 
-        <p>Der Term ist: {{ term }} </p>
+        <p>Der Term ist: {{ terme }} </p>
     </div>
 </template>
 
