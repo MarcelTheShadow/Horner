d4417bacc2bbd5a5d6a07cfdd7b12e900d976832
MarcelTheShadow <185245624+MarcelTheShadow@users.noreply.github.com>
2025-05-05 19:19:49 +0200

    little app.vue change

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
 <script setup>
-import interFace from './components/interface.vue'
+import interFace from './components/interFace.vue'
 </script>
 
 <template>
   <div class="App">
     <interFace/>
   </div>
-  
 </template>
 
 <style scoped>
