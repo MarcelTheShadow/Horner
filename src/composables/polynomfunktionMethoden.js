59fac51a2aabb6dacf348f56cd1e000879b48213
MarcelTheShadow <185245624+MarcelTheShadow@users.noreply.github.com>
2025-06-04 19:36:00 +0200

    let prettify do intendation magic

--- src/composables/polynomfunktionMethoden.js
+++ src/composables/polynomfunktionMethoden.js
@@ -1,16 +1,16 @@
-import { ref } from 'vue';
+import { ref } from "vue";
 
 // Direkter Wert der Polynomfunktion aus Input über v-model
-export const eingabeFunktion = ref('');
+export const eingabeFunktion = ref("");
 
-// Direkter Wert der Nullstelle aus Input über v-model 
-export const eingabeNullstelle = ref('');
+// Direkter Wert der Nullstelle aus Input über v-model
+export const eingabeNullstelle = ref("");
 
 // Polynomfunktion zum Zeitpunkt des Drückens des Buttons
-export const polynomfunktion = ref('');
+export const polynomfunktion = ref("");
 
 // Nullstelle zum Zeitpunkt des Drückens des Buttons
-export const nullstelle = ref('');
+export const nullstelle = ref("");
 
 // Ist Input eine gültige Polynomfunktion?
 export const funktionGueltig = ref(false);
@@ -19,7 +19,7 @@ export const funktionGueltig = ref(false);
 export const nullstelleGueltig = ref(false);
 
 // Zwischenstand von eingegebeneFunktion, um den Input zu verarbeiten
-const zwischenstand = ref('');
+const zwischenstand = ref("");
 
 // Arrays für Vorzeichen, Koeffizienten und Exponenten beim Verarbeiten der Polynomfunktion
 const vorzeichen = ref([]);
@@ -30,43 +30,45 @@ const exponenten = ref([]);
 const koeffizientenVollAufbereitet = ref([]);
 
 // Ausgabe auf der Webseite
-export const ausgabe = ref('');
-
+export const ausgabe = ref("");
 
 // Funktion, die den Wert des Inputs aktualisiert und überprüft, ob es sich um eine gültige Polynomfunktion und eine gültige Nullstelle handelt
 
- const inputGueltigOderNicht = () => {
-        // Standardmäßig ist die Funktion gültig, prüfe mittels RegEx und weiterer Logik, ob sie es tatsächlich ist 
-        funktionGueltig.value = true;
+const inputGueltigOderNicht = () => {
+    // Standardmäßig ist die Funktion gültig, prüfe mittels RegEx und weiterer Logik, ob sie es tatsächlich ist
+    funktionGueltig.value = true;
 
-        // Regulärer Ausdruck einer Polynomfunktion mit viel Flexibilität
-        if((/^\s*[+-]?\s*\d*\s*(\*?\s*x(\s*\^\s*\d+)?)?(\s*[+-]\s*\d*\s*(\*?\s*x(\s*\^\s*\d+)?)?\s*)*$/.test(zwischenstand.value)) === false) {
-            funktionGueltig.value = false;   
-        }
-        // Überprüft, ob der Input nur aus '+' und '-' besteht
-        if (zwischenstand.value.match(/^[+-\s]*$/)) {
-            funktionGueltig.value = false;
-        }
-        const inputVerarbeitet = zwischenstand.value.replace(/\s/g, '');
-        // Überprüft, ob der Input mit einem '+' oder '-' endet
-        if (inputVerarbeitet[inputVerarbeitet.length - 1] === '+' || inputVerarbeitet[inputVerarbeitet.length - 1] === '-') {
-            funktionGueltig.value = false;
-        }
-        // Nullstelle sollte eine natürliche Zahl sein, keine weiteren Zeichen erlaubt
-        if(/^-?\d+$/.test(eingabeNullstelle.value)){
-        nullstelleGueltig.value = true;
-        } else {
-            nullstelleGueltig.value = false;
-        }
+    // Regulärer Ausdruck einer Polynomfunktion mit viel Flexibilität
+    if (
+        /^\s*[+-]?\s*\d*\s*(\*?\s*x(\s*\^\s*\d+)?)?(\s*[+-]\s*\d*\s*(\*?\s*x(\s*\^\s*\d+)?)?\s*)*$/.test(
+            zwischenstand.value
+        ) === false
+    ) {
+        funktionGueltig.value = false;
     }
-
-    
-
-    
+    // Überprüft, ob der Input nur aus '+' und '-' besteht
+    if (zwischenstand.value.match(/^[+-\s]*$/)) {
+        funktionGueltig.value = false;
+    }
+    const inputVerarbeitet = zwischenstand.value.replace(/\s/g, "");
+    // Überprüft, ob der Input mit einem '+' oder '-' endet
+    if (
+        inputVerarbeitet[inputVerarbeitet.length - 1] === "+" ||
+        inputVerarbeitet[inputVerarbeitet.length - 1] === "-"
+    ) {
+        funktionGueltig.value = false;
+    }
+    // Nullstelle sollte eine natürliche Zahl sein, keine weiteren Zeichen erlaubt
+    if (/^-?\d+$/.test(eingabeNullstelle.value)) {
+        nullstelleGueltig.value = true;
+    } else {
+        nullstelleGueltig.value = false;
+    }
+};
 
 // Liest einzelne Terme und Werte aus RegEx aus und wandelt sie in Format um, das für das Auslesen der Vorzeichen, Koeffizienten und Exponenten geeignet ist
 
- const polynomfunktionAuslesen = () => {
+const polynomfunktionAuslesen = () => {
     // Hier sollen die einzelnen Terme gespeichert werden
     const terme = ref([]);
     terme.value = [];
@@ -74,29 +76,43 @@ export const ausgabe = ref('');
     // Entfernt alle Leerzeichen aus dem Input
     let i = 0;
     while (i < zwischenstand.value.length) {
-        if (zwischenstand.value[i] === ' ') {
-            zwischenstand.value = zwischenstand.value.slice(0, i) + zwischenstand.value.slice(i + 1);
+        if (zwischenstand.value[i] === " ") {
+            zwischenstand.value =
+                zwischenstand.value.slice(0, i) +
+                zwischenstand.value.slice(i + 1);
             i--;
         }
         i++;
     }
     // Fügt dem Input ein '+' vor, wenn der erste Buchstabe kein '+' oder '-' ist, damit erster Term auch erkannt wird
-    if (zwischenstand.value[0] != '+' && zwischenstand.value[0] != '-') {
-        zwischenstand.value = '+' + zwischenstand.value;
+    if (zwischenstand.value[0] != "+" && zwischenstand.value[0] != "-") {
+        zwischenstand.value = "+" + zwischenstand.value;
     }
     // Fügt dem Input ein '*' oder '1*' vor dem 'x' hinzu, wenn es nicht schon da ist
     i = 0;
     let nachVorzeichen = false;
     while (i < zwischenstand.value.length) {
-        if (zwischenstand.value[i] === '+' || zwischenstand.value[i] === '-') {
+        if (zwischenstand.value[i] === "+" || zwischenstand.value[i] === "-") {
             nachVorzeichen = true;
         }
-        if (nachVorzeichen && zwischenstand.value[i] === 'x' && zwischenstand.value[i - 1] != '*') {
-            if (zwischenstand.value[i - 1] === '+' || zwischenstand.value[i - 1] === '-') {
-                zwischenstand.value = zwischenstand.value.slice(0, i) + '1*' + zwischenstand.value.slice(i);
-            }
-            else {
-                zwischenstand.value = zwischenstand.value.slice(0, i) + '*' + zwischenstand.value.slice(i);
+        if (
+            nachVorzeichen &&
+            zwischenstand.value[i] === "x" &&
+            zwischenstand.value[i - 1] != "*"
+        ) {
+            if (
+                zwischenstand.value[i - 1] === "+" ||
+                zwischenstand.value[i - 1] === "-"
+            ) {
+                zwischenstand.value =
+                    zwischenstand.value.slice(0, i) +
+                    "1*" +
+                    zwischenstand.value.slice(i);
+            } else {
+                zwischenstand.value =
+                    zwischenstand.value.slice(0, i) +
+                    "*" +
+                    zwischenstand.value.slice(i);
             }
             nachVorzeichen = false;
         }
@@ -105,8 +121,14 @@ export const ausgabe = ref('');
     // Fügt dem Input ein '^1' hinzu, wenn es nicht schon da ist
     i = 0;
     while (i < zwischenstand.value.length) {
-        if (zwischenstand.value[i] === 'x' && zwischenstand.value[i + 1] != '^') {
-            zwischenstand.value = zwischenstand.value.slice(0, i + 1) + '^1' + zwischenstand.value.slice(i + 1);
+        if (
+            zwischenstand.value[i] === "x" &&
+            zwischenstand.value[i + 1] != "^"
+        ) {
+            zwischenstand.value =
+                zwischenstand.value.slice(0, i + 1) +
+                "^1" +
+                zwischenstand.value.slice(i + 1);
         }
         i++;
     }
@@ -116,14 +138,21 @@ export const ausgabe = ref('');
     let xIstVorhanden = false;
     while (i < zwischenstand.value.length) {
         xIstVorhanden = false;
-        while (zwischenstand.value[i] != '+' && zwischenstand.value[i] != '-' && i < zwischenstand.value.length) {
-            if (zwischenstand.value[i] === 'x') {
+        while (
+            zwischenstand.value[i] != "+" &&
+            zwischenstand.value[i] != "-" &&
+            i < zwischenstand.value.length
+        ) {
+            if (zwischenstand.value[i] === "x") {
                 xIstVorhanden = true;
             }
             i++;
         }
         if (!xIstVorhanden) {
-            zwischenstand.value = zwischenstand.value.slice(0, i) + '*x^0' + zwischenstand.value.slice(i);
+            zwischenstand.value =
+                zwischenstand.value.slice(0, i) +
+                "*x^0" +
+                zwischenstand.value.slice(i);
             i += 5;
         }
     }
@@ -132,15 +161,15 @@ export const ausgabe = ref('');
 
     for (const match of zwischenstand.value.matchAll(einzelnerTerm)) {
         if (match) {
-            terme.value += match + ',';
+            terme.value += match + ",";
         }
     }
     zwischenstand.value = terme.value;
-}
+};
 
 // Konvertiert die Polynomfunktion in Arrays für Vorzeichen, Koeffizienten und Exponenten
 
- const polynomfunktionZuArrays = () => {
+const polynomfunktionZuArrays = () => {
     // Arrays für Vorzeichen, Koeffizienten und Exponenten
     vorzeichen.value = [];
     koeffizienten.value = [];
@@ -161,11 +190,11 @@ export const ausgabe = ref('');
         }
         i++;
     }
-}
+};
 
 // Sortiert die Arrays für Vorzeichen, Koeffizienten und Exponenten nach den Exponenten
 
- const arraysSortieren = () => {
+const arraysSortieren = () => {
     // Höchster Exponent in gesamter Polynomfunktion
     let highestExponent = 0;
     for (let i = 0; i < exponenten.value.length; i++) {
@@ -185,7 +214,7 @@ export const ausgabe = ref('');
     //Füge Koeffizienten termweise aus dem Array von polynomfunktionZuArrays in das Array sortedKoeffizienten ein
     for (let i = 0; i < exponenten.value.length; i++) {
         let index = exponenten.value[i];
-        if (vorzeichen.value[i] === '+') {
+        if (vorzeichen.value[i] === "+") {
             sortierteKoeffizienten.value[index] += koeffizienten.value[i];
         } else {
             sortierteKoeffizienten.value[index] -= koeffizienten.value[i];
@@ -197,70 +226,84 @@ export const ausgabe = ref('');
 
     koeffizientenVollAufbereitet.value = [];
     koeffizientenVollAufbereitet.value = sortierteKoeffizienten.value;
-}
+};
 
- const nullstelleVerifizieren = () => {
+const nullstelleVerifizieren = () => {
     const sum = ref(0);
     for (let i = 0; i < koeffizientenVollAufbereitet.value.length; i++) {
-        sum.value += koeffizientenVollAufbereitet.value[i] * (Math.pow(parseInt(nullstelle.value), koeffizientenVollAufbereitet.value.length - i - 1));
+        sum.value +=
+            koeffizientenVollAufbereitet.value[i] *
+            Math.pow(
+                parseInt(nullstelle.value),
+                koeffizientenVollAufbereitet.value.length - i - 1
+            );
     }
     if (sum.value != 0) {
         nullstelleGueltig.value = false;
     }
-}
+};
 
 // Auf Basis der sortierten Daten von arraysSortieren wird das Horner-Schema ausgeführt
 
 const hornerSchema = () => {
     const tmp = ref(0);
     for (let i = 0; i < koeffizientenVollAufbereitet.value.length; i++) {
-        koeffizientenVollAufbereitet.value[i] = koeffizientenVollAufbereitet.value[i] + tmp.value;
-        tmp.value = koeffizientenVollAufbereitet.value[i] * parseInt(nullstelle.value);
+        koeffizientenVollAufbereitet.value[i] =
+            koeffizientenVollAufbereitet.value[i] + tmp.value;
+        tmp.value =
+            koeffizientenVollAufbereitet.value[i] * parseInt(nullstelle.value);
     }
-    koeffizientenVollAufbereitet.value = koeffizientenVollAufbereitet.value.slice(0, koeffizientenVollAufbereitet.value.length-1);
-}
+    koeffizientenVollAufbereitet.value =
+        koeffizientenVollAufbereitet.value.slice(
+            0,
+            koeffizientenVollAufbereitet.value.length - 1
+        );
+};
 
 // Umwandlung des Koeffizienten-Arrays zu lesebarer Funktion
 
 const ergebnisZuString = () => {
-const ausgabeTmp = ref('');
-const highestExponent = ref(koeffizientenVollAufbereitet.value.length - 1);
-for (let i = 0; i < koeffizientenVollAufbereitet.value.length; i++) {
-    if (koeffizientenVollAufbereitet.value[i] != 0) {
-        if (koeffizientenVollAufbereitet.value[i] > 0) {
-            ausgabeTmp.value += ' +';
-        }
-        else {
-            ausgabeTmp.value += ' ';
-        }
-        if (highestExponent.value - i === 0) {
-            ausgabeTmp.value += koeffizientenVollAufbereitet.value[i];
-        }
-        else if (highestExponent.value - i === 1) {
-            ausgabeTmp.value += koeffizientenVollAufbereitet.value[i] + 'x';
-        }
-        else {
-            ausgabeTmp.value += koeffizientenVollAufbereitet.value[i] + 'x^' + (highestExponent.value - i);
+    const ausgabeTmp = ref("");
+    const highestExponent = ref(koeffizientenVollAufbereitet.value.length - 1);
+    for (let i = 0; i < koeffizientenVollAufbereitet.value.length; i++) {
+        if (koeffizientenVollAufbereitet.value[i] != 0) {
+            if (koeffizientenVollAufbereitet.value[i] > 0) {
+                ausgabeTmp.value += " +";
+            } else {
+                ausgabeTmp.value += " ";
+            }
+            if (highestExponent.value - i === 0) {
+                ausgabeTmp.value += koeffizientenVollAufbereitet.value[i];
+            } else if (highestExponent.value - i === 1) {
+                ausgabeTmp.value += koeffizientenVollAufbereitet.value[i] + "x";
+            } else {
+                ausgabeTmp.value +=
+                    koeffizientenVollAufbereitet.value[i] +
+                    "x^" +
+                    (highestExponent.value - i);
+            }
         }
     }
-}
 
-// Kann für mehrfaches Horner-Schema bei Ableitungen zur Differenzierbarkeit genutzt werden verwendet werden!
-    if (ausgabeTmp.value === '') {
-        ausgabeTmp.value = '0';
+    // Kann für mehrfaches Horner-Schema bei Ableitungen zur Differenzierbarkeit genutzt werden verwendet werden!
+    if (ausgabeTmp.value === "") {
+        ausgabeTmp.value = "0";
     }
 
-// Ausgabe
-ausgabe.value = `${ausgabeTmp.value}`;
-}
+    // Ausgabe
+    ausgabe.value = `${ausgabeTmp.value}`;
+};
 
 // Anstatt Polynomdivison für Auslesen des Funktionswertes (der Ableitung)
 
 const funktionswertZuString = () => {
     //TODO Fakultät
-    ausgabe.value = `${koeffizientenVollAufbereitet.value[koeffizientenVollAufbereitet.value.length-1]}`;
-}
-
+    ausgabe.value = `${
+        koeffizientenVollAufbereitet.value[
+            koeffizientenVollAufbereitet.value.length - 1
+        ]
+    }`;
+};
 
 // Verarbeitet den Input, führt obige Funktionen aus
 export const eingabeVerarbeiten = () => {
@@ -276,4 +319,4 @@ export const eingabeVerarbeiten = () => {
     //hornerSchema();
     ergebnisZuString();
     //funktionswertZuString();
-}
+};
